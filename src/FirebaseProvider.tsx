73571702ad5548b652d539fffea07b17581f4881
Calum Moore <calum.moore@gmail.com>
2020-06-22 01:11:33 +0100

    Single handler for auth changes

--- src/FirebaseProvider.tsx
+++ src/FirebaseProvider.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const FirebaseContext = React.createContext<firebase.app.App|null>(null)
+export const FirebaseAuthContext = React.createContext<[firebase.User|null, boolean]>([null, true])
 
 interface FirebaseProviderProps {
   app: firebase.app.App
@@ -8,9 +9,19 @@ interface FirebaseProviderProps {
 }
 
 export default function FirebaseProvider ({ children, app }: FirebaseProviderProps) {
+  const [state, setState] = useState<[firebase.User | null, boolean]>([null, true])
+    
+  useEffect(() => {
+    return app.auth().onAuthStateChanged(async (user) => {
+      setState([user, false])
+    })
+  }, [app.auth])
+  
   return (
     <FirebaseContext.Provider value={app}>
-      {children}
+      <FirebaseAuthContext.Provider value={state}>
+        {children}
+      </FirebaseAuthContext.Provider>
     </FirebaseContext.Provider>
   )
 }
--- src/useFirebaseAuth.ts
+++ src/useFirebaseAuth.ts
@@ -1,18 +1,6 @@
-import { useEffect, useState } from 'react'
-import useFirebase from './useFirebase'
-import 'firebase/app'
+import { useContext } from 'react'
+import { FirebaseAuthContext } from './FirebaseProvider'
 
-export default function useFirebaseAuth (): [firebase.User | null, boolean] {
-  const [state, setState] = useState<[firebase.User | null, boolean]>([null, true])
-  const firebase = useFirebase()
-
-  useEffect(() => {
-    if (!firebase) return
-
-    return firebase.auth().onAuthStateChanged(async (user) => {
-      setState([user, false])
-    })
-  }, [firebase])
-
-  return state
+export default function useFirebase () {
+  return useContext(FirebaseAuthContext)
 }
